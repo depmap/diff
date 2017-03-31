42feb59b7b25d397bd3e943775cb90f1948c05da
Henry Snopek <hhsnopek@gmail.com>
2017-03-31 01:00:10 -0400

    async -> sync

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const tree = require('treeify')
+const fs = require('fs')
+const path = require('path')
+const process = require('process')
 
-module.exports = (deps, file, opts) => {
-	console.log(tree.asTree(deps))
+module.exports = (file, opts) => {
+  file = path.join(process.cwd(), file)
+  let meta = path.parse(file)
+
+  // TODO check if opts.load[meta.ext.substring(1)] exists
+  // if not, check if it wasn't properly named by iterating over
+  // opts.load[iterator].meta.ext
+  let loader = opts.load[meta.ext.substring(1)]
+
+  console.log('compiling', file)
+  let data = loader.compile.file(file)
+  let targetExt = loader.meta.outExt
+  if (!fs.existsSync(opts.output)) fs.mkdirSync(opts.output)
+  let target = path.join(opts.output, `${meta.name}.${targetExt}`)
+  fs.writeFileSync(target, data)
 }
