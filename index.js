a0d504f9802db70e7c15a59b18003b6aca4f68cf
Henry Snopek <hhsnopek@gmail.com>
2017-04-04 21:02:51 -0400

    stablize

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ const process = require('process')
 module.exports = (file, opts) => {
   file = path.join(process.cwd(), file)
   let meta = path.parse(file)
+  // ignore '_'
+  if (meta.base.indexOf('_') === 0) return
 
   // TODO check if opts.load[meta.ext.substring(1)] exists
   // if not, check if it wasn't properly named by iterating over
   // opts.load[iterator].meta.ext
-  let loader = opts.load[meta.ext.substring(1)]
+  let type = meta.ext.substring(1)
+  let loader = opts.load[type]
 
-  console.log('compiling', file)
-  let data = loader.compile.file(file)
+  console.log(`compiling ${meta.name}${meta.ext}`)
+  let data = loader.compile.file(file, opts[type])
   let targetExt = loader.meta.outExt
+  let targetDir = ''
   if (!fs.existsSync(opts.output)) fs.mkdirSync(opts.output)
-  let target = path.join(opts.output, `${meta.name}.${targetExt}`)
+  if (loader.meta.outDir && loader.meta.outDir !== '') {
+    targetDir = path.join(opts.output, targetExt)
+    if (!fs.existsSync(targetDir)) fs.mkdirSync(targetDir)
+  }
+  let target = path.join(targetDir,`${meta.name}.${targetExt}`)
   fs.writeFileSync(target, data)
+  console.log(`  - done: ${target}`)
 }
